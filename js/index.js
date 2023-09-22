587b935a71a52a53e4001a77f4250742ab26434e
Honza Cieslar <109418897+honzawashere@users.noreply.github.com>
2023-09-22 13:09:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let last_choice;
+
 function click_me_clicked() {
     document.querySelector("#page_container").classList.add("page-container-hidden")
     document.querySelector("#particles-js").classList.add("particles-hide")
@@ -72,12 +74,36 @@ function phone_sidebar() {
         document.querySelector("#open_button").classList.add("hidden")
 
         document.querySelector("#menu_list_phone").classList.remove("hidden")
+
+        if(!document.querySelector("#description_1").classList.includes("description_hidden") {
+            last_choice = 1
+        }
+        if(!document.querySelector("#description_2").classList.includes("description_hidden") {
+            last_choice = 2
+        }
+        if(!document.querySelector("#description_3").classList.includes("description_hidden") {
+            last_choice = 3
+        }
+
+        document.querySelector("#description_1").classList.add("description-hidden")
+        document.querySelector("#description_2").classList.add("description-hidden")
+        document.querySelector("#description_3").classList.add("description-hidden")
     }
     document.querySelector("#close_button").onclick = () => {
         document.querySelector("#close_button").classList.add("hidden")
         document.querySelector("#open_button").classList.remove("hidden")
 
         document.querySelector("#menu_list_phone").classList.add("hidden")
+
+        if(last_choice === 1) {
+            document.querySelector("#description_1").classList.remove("description_hidden")
+        }
+        if(last_choice === 2) {
+            document.querySelector("#description_2").classList.remove("description_hidden")
+        }
+        if(last_choice === 3) {
+            document.querySelector("#description_3").classList.remove("description_hidden")
+        }
     }
     document.querySelector("#menu_option_1_phone").onclick = () => {
         document.querySelector("#menu_option_1").classList.add("option-selected")
