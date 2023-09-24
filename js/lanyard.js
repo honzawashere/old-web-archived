16bd6f1333d96f5d1253cdf1ea6c2eaad580aeea
Honza Cieslar <109418897+honzawashere@users.noreply.github.com>
2023-09-24 15:30:25 +0200

    Add files via upload

--- js/lanyard.js
+++ js/lanyard.js
@@ -7,6 +7,16 @@ function make_lanyard_work() {
         console.log(`[LANYARD] Connection opened`)
     })
 
+    function c() {
+        let xhr = new XMLHttpRequest()
+        xhr.open("GET", "https://fourhbfij6upux8ujwei9zvr8qemfhmrz.onrender.com/color")
+        xhr.onreadystatechange = () => {
+            document.querySelector("#activity_container").style.background = xhr.response
+            document.querySelector("#phone_activity_container").style.background = xhr.response
+        }
+        xhr.send()
+    }
+
     socket.addEventListener("message", (event) => {
         const data = JSON.parse(event.data)
         if (data.op == 1) {
@@ -21,16 +31,48 @@ function make_lanyard_work() {
                 const d = data.d["900801159424512020"]
                 if (d.activities.length === 0) return document.querySelector("#activity_container").classList.add("hidden")
                 if (d.activities[0].id !== "spotify:1") {
-                    const a = d.activities[0]
-                    document.querySelector("#activity_container").classList.remove("hidden")
-                    document.querySelector("#activity_title").innerHTML = "My Activity"
-                    document.querySelector("#activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
-                    document.querySelector(`#activity_name`).innerHTML = a.name
-                    document.querySelector(`#activity_state`).innerHTML = a.state
-                    document.querySelector(`#activity_state`).classList.add("no-hover")
-                }
-                else {
-                    const a = d.activities[0]
+                    if (d.activities[0].id === "ef12ade3b96ec3d4") {
+                        const a = d.activities[0]
+                        setTimeout(() => {
+                            c()
+                        }, 1000)
+                        document.querySelector("#activity_container").classList.remove("hidden")
+                        document.querySelector("#activity_title").innerHTML = "Listening on YouTube Music"
+                        document.querySelector("#activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector("#activity_image").setAttribute("src", `https://${a.assets.large_image.split("https/")[1]}`)
+                        document.querySelector(`#activity_name`).innerHTML = a.state.replace(";", ",")
+                        document.querySelector(`#activity_state`).innerHTML = a.details
+                        document.querySelector(`#activity_state`).classList.add("no-hover")
+
+                        document.querySelector("#phone_activity_container").classList.remove("hidden")
+                        document.querySelector("#phone_activity_title").innerHTML = "Listening on YouTube Music"
+                        document.querySelector("#phone_activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector("#phone_activity_image").setAttribute("src", `https://${a.assets.large_image.split("https/")[1]}`)
+                        document.querySelector(`#phone_activity_name`).innerHTML = a.state.replace(";", ",")
+                        document.querySelector(`#phone_activity_state`).innerHTML = a.details
+                        document.querySelector(`#phone_activity_state`).classList.add("no-hover")
+                    } else {
+                        const a = d.activities[0]
+                        document.querySelector("#activity_container").style.background = "#000000"
+                        document.querySelector("#activity_container").classList.remove("hidden")
+                        document.querySelector("#activity_title").innerHTML = "My Activity"
+                        document.querySelector("#activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector("#activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
+                        document.querySelector(`#activity_name`).innerHTML = a.name
+                        document.querySelector(`#activity_state`).innerHTML = a.state
+                        document.querySelector(`#activity_state`).classList.add("no-hover")
+
+                        document.querySelector("#phone_activity_container").style.background = "#000000"
+                        document.querySelector("#phone_activity_container").classList.remove("hidden")
+                        document.querySelector("#phone_activity_title").innerHTML = "My Activity"
+                        document.querySelector("#phone_activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector("#phone_activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
+                        document.querySelector(`#phone_activity_name`).innerHTML = a.name
+                        document.querySelector(`#phone_activity_state`).innerHTML = a.state
+                        document.querySelector(`#phone_activity_state`).classList.add("no-hover")
+                    }
+                } else {
+                    document.querySelector("#activity_container").style.background = "#000000"
                     document.querySelector("#activity_container").classList.remove("hidden")
                     document.querySelector("#activity_title").innerHTML = "Listening to Spotify"
                     document.querySelector("#activity_image").setAttribute("src", `https://i.scdn.co/image/${a.assets.large_image.replace("spotify:", "")}`)
@@ -38,29 +80,80 @@ function make_lanyard_work() {
                     document.querySelector(`#activity_state`).innerHTML = a.details
                     document.querySelector(`#activity_state`).classList.remove("no-hover")
                     document.querySelector(`#activity_state`).setAttribute("href", "https://open.spotify.com/track/" + d.spotify.track_id)
+
+                    document.querySelector("#phone_activity_container").style.background = "#000000"
+                    document.querySelector("#phone_activity_container").classList.remove("hidden")
+                    document.querySelector("#phone_activity_title").innerHTML = "Listening to Spotify"
+                    document.querySelector("#phone_activity_image").setAttribute("src", `https://i.scdn.co/image/${a.assets.large_image.replace("spotify:", "")}`)
+                    document.querySelector(`#phone_activity_name`).innerHTML = a.state.replace(";", ",")
+                    document.querySelector(`#phone_activity_state`).innerHTML = a.details
+                    document.querySelector(`#phone_activity_state`).classList.remove("no-hover")
+                    document.querySelector(`#phone_activity_state`).setAttribute("href", "https://open.spotify.com/track/" + d.spotify.track_id)
                 }
             }
             if (data.t === "PRESENCE_UPDATE") {
                 const d = data.d
                 if (d.activities.length === 0) return document.querySelector("#activity_container").classList.add("hidden")
-                if (d.activities[0].id !== "spotify:1") {
-                    document.querySelector("#activity_container").classList.remove("hidden")
+                if (d.activities[0].id === "ef12ade3b96ec3d4") {
                     const a = d.activities[0]
-                    document.querySelector("#activity_title").innerHTML = "My Activity"
-                    document.querySelector("#activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
-                    document.querySelector(`#activity_name`).innerHTML = a.name
-                    document.querySelector(`#activity_state`).innerHTML = a.state
-                    document.querySelector(`#activity_state`).classList.add("no-hover")
-                }
-                else {
-                    const a = d.activities[0]
+                    setTimeout(() => {
+                        c()
+                    }, 1000)
                     document.querySelector("#activity_container").classList.remove("hidden")
-                    document.querySelector("#activity_title").innerHTML = "Listening to Spotify"
-                    document.querySelector("#activity_image").setAttribute("src", `https://i.scdn.co/image/${a.assets.large_image.replace("spotify:", "")}`)
+                    document.querySelector("#activity_title").innerHTML = "Listening on YouTube Music"
+                    document.querySelector("#activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                    document.querySelector("#activity_image").setAttribute("src", `https://${a.assets.large_image.split("https/")[1]}`)
                     document.querySelector(`#activity_name`).innerHTML = a.state.replace(";", ",")
                     document.querySelector(`#activity_state`).innerHTML = a.details
-                    document.querySelector(`#activity_state`).classList.remove("no-hover")
-                    document.querySelector(`#activity_state`).setAttribute("href", "https://open.spotify.com/track/" + d.spotify.track_id)
+                    document.querySelector(`#activity_state`).classList.add("no-hover")
+
+                    document.querySelector("#phone_activity_container").classList.remove("hidden")
+                    document.querySelector("#phone_activity_title").innerHTML = "Listening on YouTube Music"
+                    document.querySelector("#phone_activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                    document.querySelector("#phone_activity_image").setAttribute("src", `https://${a.assets.large_image.split("https/")[1]}`)
+                    document.querySelector(`#phone_activity_name`).innerHTML = a.state.replace(";", ",")
+                    document.querySelector(`#phone_activity_state`).innerHTML = a.details
+                    document.querySelector(`#phone_activity_state`).classList.add("no-hover")
+                } else {
+                    if (d.activities[0].id !== "spotify:1") {
+                        document.querySelector("#activity_container").classList.remove("hidden")
+                        document.querySelector("#activity_container").style.background = "#000000"
+                        const a = d.activities[0]
+                        document.querySelector("#activity_title").innerHTML = "My Activity"
+                        document.querySelector("#activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
+                        document.querySelector("#activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector(`#activity_name`).innerHTML = a.name
+                        document.querySelector(`#activity_state`).innerHTML = a.state
+                        document.querySelector(`#activity_state`).classList.add("no-hover")
+
+                        document.querySelector("#phone_activity_container").classList.remove("hidden")
+                        document.querySelector("#phone_activity_container").style.background = "#000000"
+                        document.querySelector("#phone_activity_title").innerHTML = "My Activity"
+                        document.querySelector("#phone_activity_image").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.large_image}.png`)
+                        document.querySelector("#phone_activity_image_small").setAttribute("src", `https://cdn.discordapp.com/app-assets/${a.application_id}/${a.assets.small_image}.png`)
+                        document.querySelector(`#phone_activity_name`).innerHTML = a.name
+                        document.querySelector(`#phone_activity_state`).innerHTML = a.state
+                        document.querySelector(`#phone_activity_state`).classList.add("no-hover")
+                    } else {
+                        const a = d.activities[0]
+                        document.querySelector("#activity_container").style.background = "#000000"
+                        document.querySelector("#activity_container").classList.remove("hidden")
+                        document.querySelector("#activity_title").innerHTML = "Listening to Spotify"
+                        document.querySelector("#activity_image").setAttribute("src", `https://i.scdn.co/image/${a.assets.large_image.replace("spotify:", "")}`)
+                        document.querySelector(`#activity_name`).innerHTML = a.state.replace(";", ",")
+                        document.querySelector(`#activity_state`).innerHTML = a.details
+                        document.querySelector(`#activity_state`).classList.remove("no-hover")
+                        document.querySelector(`#activity_state`).setAttribute("href", "https://open.spotify.com/track/" + d.spotify.track_id)
+
+                        document.querySelector("#phone_activity_container").style.background = "#000000"
+                        document.querySelector("#phone_activity_container").classList.remove("hidden")
+                        document.querySelector("#phone_activity_title").innerHTML = "Listening to Spotify"
+                        document.querySelector("#phone_activity_image").setAttribute("src", `https://i.scdn.co/image/${a.assets.large_image.replace("spotify:", "")}`)
+                        document.querySelector(`#phone_activity_name`).innerHTML = a.state.replace(";", ",")
+                        document.querySelector(`#phone_activity_state`).innerHTML = a.details
+                        document.querySelector(`#phone_activity_state`).classList.remove("no-hover")
+                        document.querySelector(`#phone_activity_state`).setAttribute("href", "https://open.spotify.com/track/" + d.spotify.track_id)
+                    }
                 }
             }
         }
